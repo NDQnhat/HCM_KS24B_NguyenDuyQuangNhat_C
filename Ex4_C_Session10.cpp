b558ed5928f9e97c71d1eab016f6a3c02500d92a
NDQnhat <ngduyquangnhat03@gmail.com>
2024-12-23 11:44:32 +0700

    Update Ex4_C_Session10.cpp

--- Ex4_C_Session10.cpp
+++ Ex4_C_Session10.cpp
@@ -2,16 +2,16 @@
 int main() {
 	int array[]={9,5,77,24,31,67,0,12};
 	int length=sizeof(array)/sizeof(int);
-	for (int i=0;i<length;i++){
-		int minIndex=i;
-		for(int j=i+1;j<length;j++){
-			if(array[j]<array[minIndex]){
-				minIndex=j;
-			}
-		} int temp=array[minIndex];
-		array[minIndex]=array[i];
-		array[i]=temp;
-	} printf("\nmang sau sap xep: ");
+	for(int i=0;i<n-1;i++){
+        int min=arr[i];
+        for(int j=i+1;j<n;j++){
+            if(min>arr[j]){
+                min=arr[j];
+                arr[j]=arr[i];
+                arr[i]=min;
+            }
+        }
+        } printf("\nmang sau sap xep: ");
 	for(int i=0;i<length;i++){
 		printf("%d ",array[i]);
 	}return 0;

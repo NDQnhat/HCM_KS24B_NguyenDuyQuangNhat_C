f47089d2ec051cd0f527d4d61b8435c63d665968
NDQnhat <ngduyquangnhat03@gmail.com>
2024-12-24 10:08:52 +0700

    Update Ex4_C_Session10.cpp

--- Ex4_C_Session10.cpp
+++ Ex4_C_Session10.cpp
@@ -3,7 +3,7 @@ int main() {
 	int array[]={9,5,77,24,31,67,0,12};
 	int length=sizeof(array)/sizeof(int);
 	for(int i=0;i<length-1;i++){
-        int min=arr[i];
+        int min=array[i];
         for(int j=i+1;j<length;j++){
             if(min>array[j]){
                 min=array[j];

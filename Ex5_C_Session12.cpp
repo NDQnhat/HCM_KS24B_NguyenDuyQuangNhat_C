6991ec87e53987e556f82b54f4784d021c3d3e74
NDQnhat <ngduyquangnhat03@gmail.com>
2024-12-28 15:18:50 +0700

    up btvn

--- Ex5_C_Session12.cpp
+++ Ex5_C_Session12.cpp
@@ -1,29 +1,33 @@
 #include <stdio.h>
 #include <math.h>
+#include <stdbool.h>
 int check(int input);
 int main() {
 	int a,b;
 	printf("nhap a,b:\n");
 	scanf("%d%d", &a,&b);
-	check(a);
-	printf("\n");
-	check(b);
-	return 0;
+    if(check(a)){
+        printf("%d la prime", a);
+    }else{
+        printf("%d khong phai prime", a);
+    } if(check(b)){
+        printf("\n%d la prime", b);
+    }else{
+        printf("\n%d khong phai prime", b);
+    }return 0;
 }
 int check(int input){
 	if(input<2){
-		printf("%d khong pahai so prime",input);
+		return false;
 	} else {
 		int flag=1;
 		for(int i=2;i<sqrt(input);i++){
 			if(input%i==0){
-				printf("%d khong phai prime", input);
 				flag=0;
-				return 0;
+				return false;
 			}
 		}if(flag){
-			printf("%d la so nguyemn to", input);
-			return 1;
+			return true;
 		}
 	} 
 }

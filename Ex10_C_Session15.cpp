8e00a926c8b1a234959a225fddd5a167a243c64d
NDQnhat <ngduyquangnhat03@gmail.com>
2025-01-02 11:15:59 +0700

    up btvn

--- Ex10_C_Session15.cpp
+++ Ex10_C_Session15.cpp
@@ -7,11 +7,11 @@ int main()
     char chr;
     for (int i = 0; i < strlen(str); i++) {
         count = 1;
-        if (str[i] == '\0') continue;
+        if (str[i] == -1) continue;
         for (int j = i + 1; j < strlen(str); j++) {
             if (str[i] == str[j]) {
                 count++;
-                str[j] = '\0';
+                str[j] = -1;
             }
         }printf("%c: %d\n", str[i], count);
     }return 0;

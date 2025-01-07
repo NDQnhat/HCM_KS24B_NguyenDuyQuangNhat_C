ba9a9fca19eb4819997ff1c9846c510e4509b591
NDQnhat <ngduyquangnhat03@gmail.com>
2025-01-07 16:17:55 +0700

    up btvn

--- Ex1_C_Session16.cpp
+++ Ex1_C_Session16.cpp
@@ -4,8 +4,8 @@ int main()
 {
     int a=0;
     int *aPtr=&a;
-    printf("gtri: %d, dia chi: %ld\n", a, (unsigned long)&a);
-    printf("gtri: %d, dia chi: %ld\n", *aPtr, (unsigned long)aPtr);
+    printf("gtri: %d, dia chi: %lu\n", a, (unsigned long)&a);
+    printf("gtri: %d, dia chi: %lu\n", *aPtr, (unsigned long)aPtr);
     return 0;
 }
 /*#include <stdio.h>
